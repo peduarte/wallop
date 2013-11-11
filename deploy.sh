981b0ee5405907eb1f1d41ba1d61ea9163730128
Pedro Duarte <contact@peduarte.com>
2013-11-11 22:32:11 +0000

    Adding new files before

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git add .
+git commit -am "Adding new files before"
+git pull origin master
+git push origin master
+git checkout gh-pages
+git merge master
+git pull origin gh-pages
+git push origin gh-pages
